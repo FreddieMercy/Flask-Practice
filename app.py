97726afdde924b7ca80f4042d6d39e461778d152
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-04-30 17:46:49 -0700

    Hello, World

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)  # only referencing this file
+@app.route('/')
+def index():
+    return "Hello, World"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
